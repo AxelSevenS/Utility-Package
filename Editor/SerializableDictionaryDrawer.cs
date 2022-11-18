1b9875612b92bbfb689b102794346ffbcde0af20
AxelSeven <spaghettiandmeatballs61@gmail.com>
2022-11-18 12:45:56 +0100

    SerializableDictionary

---
+++ Editor/SerializableDictionaryDrawer.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System;
+
+using SevenGame.Utility.Editor;
+
+namespace SeleneGame.Core {
+
+    [CustomPropertyDrawer(typeof(SerializableDictionary), true)]
+    public class SerializableDictionaryDrawer : PropertyDrawer {
+
+        private SerializedProperty _pairs = null;
+        private ReorderableList _list = null;
+
+        private bool showContents = true;
+
+
+
+        private SerializedProperty GetPairs(SerializedProperty property){
+            if (_pairs == null) {
+                _pairs = property.FindPropertyRelative("_pairs");
+            }
+            return _pairs;
+        }
+
+        private ReorderableList GetReorderableList(SerializedProperty property){
+            
+            var pairsProperty = GetPairs(property);
+
+            if (_list == null) {
+                _list = new ReorderableList(pairsProperty.serializedObject, pairsProperty, true, false, true, true);
+                _list.drawHeaderCallback = (Rect rect) => {
+                    EditorGUI.LabelField(rect, property.displayName);
+                };
+                _list.drawElementCallback = (UnityEngine.Rect rect, int index, bool isActive, bool isFocused) => {
+                    rect.width -= 10;
+                    rect.x += 10;
+                    EditorGUI.PropertyField(rect, pairsProperty.GetArrayElementAtIndex(index), true);
+                };
+                _list.onAddCallback = (ReorderableList list) => {
+                    var index = list.serializedProperty.arraySize;
+                    list.serializedProperty.arraySize++;
+                    list.index = index;
+                    var element = list.serializedProperty.GetArrayElementAtIndex(index);
+                    SerializedPropertyUtility.ResetValue(element.FindPropertyRelative("Key"));
+                };
+            }
+            return _list;
+        }
+
+
+        public override void OnGUI( Rect position, SerializedProperty property, GUIContent label ) {
+            EditorGUI.BeginProperty( position, label, property );
+
+            var pairsProperty = GetPairs(property);
+            var reorderableList = GetReorderableList(property);
+            
+            Rect foldoutPosition = new Rect(position.xMin, position.yMin, position.width, EditorGUIUtility.singleLineHeight);
+
+            showContents = EditorGUI.BeginFoldoutHeaderGroup(foldoutPosition, showContents, property.displayName);
+                if (showContents) {
+                    var height = 0f;
+                    for(var i = 0; i < pairsProperty.arraySize; i++) {
+                        height = Mathf.Max(height, EditorGUI.GetPropertyHeight(pairsProperty.GetArrayElementAtIndex(i)));
+                    }
+                    reorderableList.elementHeight = height;
+                    foldoutPosition.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                    reorderableList.DoList(foldoutPosition);
+                }
+            EditorGUI.EndFoldoutHeaderGroup();
+
+            EditorGUI.EndProperty();
+        }
+        
+        public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
+    
+            float height = EditorGUIUtility.singleLineHeight;
+            if (showContents)
+                height += GetReorderableList(property).GetHeight() + EditorGUIUtility.standardVerticalSpacing;
+
+            return height;
+        }
+    }
+}
---
+++ Runtime/SerializableDictionary.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SevenGame.Utility;
+using System.Linq;
+
+namespace SeleneGame.Core {
+
+    public abstract class SerializableDictionary {}
+
+    [System.Serializable]
+    public class SerializableDictionary<TKey, TValue> : SerializableDictionary, IDictionary<TKey, TValue>, ISerializationCallbackReceiver {
+
+        [SerializeField] private List<ValuePair<TKey, TValue>> _pairs = new List<ValuePair<TKey, TValue>>();
+        private Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
+        
+
+        public ICollection<TKey> Keys => _dictionary.Keys;
+
+        public ICollection<TValue> Values => _dictionary.Values;
+
+        public int Count => _dictionary.Count;
+
+        public bool IsReadOnly => false;
+
+        public TValue this[TKey key] { 
+            get => _dictionary[key];
+            set => _dictionary[key] = value;
+        }
+
+
+        public bool ContainsKey(TKey key) {
+            return _dictionary.ContainsKey(key);
+        }
+
+        bool IDictionary<TKey, TValue>.Remove(TKey key) {
+            return _dictionary.Remove(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value) {
+            return _dictionary.TryGetValue(key, out value);
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item) {
+            _dictionary.Add(item.Key, item.Value);
+        }
+
+        public void Add(TKey key, TValue value) {
+            _dictionary.Add(key, value);
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item) {
+            return _dictionary.Remove(item.Key);
+        }
+
+        public void Remove(TKey key) {
+            _dictionary.Remove(key);
+        }
+        
+        public void Remove(TKey key, TValue value) {
+            Remove(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public void Clear() {
+            _dictionary.Clear();
+            _pairs.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item) {
+            return _dictionary.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
+            array = _dictionary.ToArray();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return _dictionary.GetEnumerator();
+        }
+
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() {
+            return _dictionary.GetEnumerator();
+        }
+
+        
+        public void OnBeforeSerialize() {
+            _pairs.Clear();
+            foreach (var pair in _dictionary) {
+                _pairs.Add(new ValuePair<TKey, TValue>(pair.Key, pair.Value));
+            }
+        }
+
+        public void OnAfterDeserialize() {
+            _dictionary = new Dictionary<TKey, TValue>();
+            foreach (var pair in _pairs) {
+                try {
+                    _dictionary.Add(pair.Key, pair.Value);
+                } catch (ArgumentException) {
+                    Debug.LogWarning($"Duplicate key {pair.Key} found in {this.GetType().Name}");
+                }
+            }
+        }
+    }
+
+}
